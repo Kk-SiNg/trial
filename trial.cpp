569262a01e41fa8fdfc4a08edb443f7838de0958
KKS <kartiksingh25july@gmail.com>
2025-09-29 17:11:40 +0530

    new known changes

--- trial.cpp
+++ trial.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int main(){
     int t; 
     cin >>t;
-    cout << "printing numbers from t - 1\n";
+//commenting out
+    cout << "printing number from t - 1\n";
+    cout << "extra line just to show offf!!\n";
     while(t){
         cout << "yoyo:" << t << endl;
         t--; 
