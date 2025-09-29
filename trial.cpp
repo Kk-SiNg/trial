5801a710204e7ea210ccd8855c5e0e620d7e2ed3
KKS <kartiksingh25july@gmail.com>
2025-09-29 17:22:59 +0530

    aded 1st

--- trial.cpp
+++ trial.cpp
@@ -5,11 +5,6 @@ using namespace std;
 int main(){
     int t; 
     cin >>t;
-//commenting out
-    cout << "printing number from t - 1\n";
-    cout << "extra line just to show offf!!\n";
-    while(t){
-        cout << "yoyo:" << t << endl;
-        t--; 
-    }
+
+    cout << "1st\n";
 }
