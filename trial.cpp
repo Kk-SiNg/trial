32561137daea395fbcd827e297ed3a72ed47a9cd
KKS <kartiksingh25july@gmail.com>
2025-09-29 17:10:24 +0530

    made unknown changes in trial

--- trial.cpp
+++ trial.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     int t; 
     cin >>t;
-    cout << "printing numbers from t to 1\n";
+    cout << "printing numbers from t - 1\n";
     while(t){
         cout << "yoyo:" << t << endl;
         t--; 
